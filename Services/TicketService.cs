20184f86f3a7d81ceefbc7c08c9216f03f1b5d0a
Fifaliana_valea <valeafifaliana@gmail.com>
2025-03-24 10:00:16 +0300

    detele ticket

--- Services/TicketService.cs
+++ Services/TicketService.cs
@@ -32,30 +32,28 @@ namespace crmcsharp.Services
             }
         }
 
-        public async Task<bool> DeleteTicket(int ticketId)
-        {
-            try
-            {
-                HttpResponseMessage response = await _httpClient.DeleteAsync($"delete-ticket/{ticketId}");
+       public async Task<bool> DeleteTicket(int ticketId)
+{
+    try
+    {
+        HttpResponseMessage response = await _httpClient.DeleteAsync($"delete-ticket/{ticketId}");
 
-                if (response.IsSuccessStatusCode)
-                {
-                    return true; 
-                }
-                else if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
-                {
-                    throw new ApplicationException("Ticket non trouvé.");
-                }
-                else
-                {
-                    throw new ApplicationException($"Erreur lors de la suppression du ticket. Code de statut : {response.StatusCode}");
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new ApplicationException("Erreur lors de la suppression du ticket.", ex);
-            }
+        if (response.IsSuccessStatusCode)
+        {
+            return true; 
         }
+        else
+        {
+            string content = await response.Content.ReadAsStringAsync();
+            throw new ApplicationException($"Erreur lors de la suppression du ticket. Code: {(int)response.StatusCode} - {response.ReasonPhrase}. Contenu: {content}");
+        }
+    }
+    catch (Exception ex)
+    {
+        throw new ApplicationException("Erreur lors de la suppression du ticket.", ex);
+    }
+}
+
 
         public async Task<TicketHisto> GetTicketByIdAsync(int id)
         {
