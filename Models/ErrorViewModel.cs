f69812755119a386dc9edbb6daf1e6843e4844b6
Fifaliana_valea <valeafifaliana@gmail.com>
2025-03-26 00:16:01 +0300

    amelioration de l'affichage

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace crmcsharp.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
-
+    public string RequestId { get; set; }
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    public string ErrorMessage { get; set; }
 }
